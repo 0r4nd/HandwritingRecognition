609f0dd0185d802c755f200d52fbc1209a7abf5e
younes bouzerdoum <0r4nd@protonmail.com>
2023-06-05 21:54:48 +0200

    update App

--- API/src/App.jsx
+++ API/src/App.jsx
@@ -1,46 +1,46 @@
-import logo from '../../logo.svg';
+
+
+import { Stack } from "@mui/material"
 import './App.css';
-import DrawCanvas from "../DrawCanvas/DrawCanvas";
+import MuiButton from "./components/MuiButton/MuiButton";
+import CanvasPaint from "./components/CanvasPaint/CanvasPaint";
+import { MuiTypography } from './components/MuiTypography';
 
 var model = undefined;
 var model_aabb = [0,0,0,0];
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function predictCallback() {}
-function cleanCallback() {}
-
 //<canvas className="App-canvas-output"></canvas>
 function App() {
+
+  const handleClickRender = () => {
+    console.log("render!")
+  };
+  const handleClickClean = () => {
+    console.log("clean")
+  };
+
   return <div className="App">
-    <div display="flex">
-      <DrawCanvas/>
+
+    {/*<MuiTypography></MuiTypography>*/}
+
+    <Stack spacing={2} display='block' direction='row'>
+      <CanvasPaint/>
       <canvas className="App-canvas-output"></canvas>
-    </div>
+    </Stack>
+
     <div>Result: </div>
-    <div display="flex">
-      <button onClick={predictCallback} className="App-button">Predict</button>
-      <button onClick={cleanCallback} className="App-button">Clean</button>
-    </div>
+
+    <Stack spacing={2} display='block' direction='row'>
+      <MuiButton onClick={handleClickRender} color='secondary' variant='contained' size='large' text="Predict"/>
+      <MuiButton onClick={handleClickClean} color='secondary' variant='outlined' size='small' text="Clean"/>
+      {/*<button onClick={handleClickClean} className="App-button">Clean</button>*/}
+    </Stack>
+
   </div>
   
 }
 
+//<button onClick={predictCallback} className="App-button">Predict</button>
 export default App;
