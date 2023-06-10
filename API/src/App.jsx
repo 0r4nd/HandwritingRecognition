c8b1bec081d16e89506e4bd95542533ab39488b7
0r4nd <103434548+0r4nd@users.noreply.github.com>
2023-06-10 22:18:52 +0200

    update

--- API/src/App.jsx
+++ API/src/App.jsx
@@ -1,17 +1,22 @@
  
 import './App.css';
-import * as tf from '@tensorflow/tfjs';
-import { useState, useRef, useEffect } from "react"
-import { Stack, Paper, Card, Box, Alert } from "@mui/material"
-import MuiButton from "./components/MuiButton/MuiButton";
-import CanvasPaint from "./components/CanvasPaint/CanvasPaint";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import HandWritingPredictor from './components/HandWritingPredictor/HandWritingPredictor';
+
+/* Themes */
+//const darkTheme = createTheme({ palette: { mode: 'dark' } });
+const lightTheme = createTheme({ palette: { mode: 'light' } });
+
+/* consts */
+const modelURL = './assets/models/model_0000/model.json';
 
 
 export default function App() {
-  return <div className="App">
-    <div>Hand writing predictor model using MNIST dataset:</div>
-    <HandWritingPredictor></HandWritingPredictor>
-  </div>
+  return <ThemeProvider theme={lightTheme}>
+    <div className="App">
+      <div>Hand writing predictor model using MNIST dataset:</div>
+      <HandWritingPredictor modelURL={modelURL}></HandWritingPredictor>
+    </div>
+  </ThemeProvider>;
 }
 
